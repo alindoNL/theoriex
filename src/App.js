6c7473895120faa7b5d08a6a0f5596e7a24441bb
alindo <alindo_2002@hotmail.com>
2022-07-21 19:54:30 +0200

    routerv6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/home'
 import Admin from './components/admin/admin'
 import Theorieexamens from './pages/theorieexamens'
 import Contact from './pages/contact'
 import Examen1 from './pages/examens/examen1'
-import Dropdown from './components/dropdown';
+import Dropdown from './components/dropdown'
 import Examen2 from './pages/examens/examen2'
 import Examen3 from './pages/examens/examen3'
 import Examen4 from './pages/examens/examen4'
@@ -17,55 +17,28 @@ import Examen10 from './pages/examens/examen10'
 function App() {
   return (
     <Router>
-    
       <Dropdown />
 
-      <div>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/admin'>
-          <Admin />
-        </Route>
-        <Route path='/theorieexamens'>
-          <Theorieexamens />
-        </Route>
-        <Route path='/contact'>
-          <Contact />
-        </Route>
-        <Route path='/examen1'>
-          <Examen1 />
-        </Route>
-        <Route path='/examen2'>
-          <Examen2 />
-        </Route>
-        <Route path='/examen3'>
-          <Examen3 />
-        </Route>
-        <Route path='/examen4'>
-          <Examen4 />
-        </Route>
-        <Route path='/examen5'>
-          <Examen5 />
-        </Route>
-        <Route path='/examen6'>
-          <Examen6 />
-        </Route>
-        <Route path='/examen7'>
-          <Examen7 />
-        </Route>
-        <Route path='/examen8'>
-          <Examen8 />
-        </Route>
-        <Route path='/examen9'>
-          <Examen9 />
-        </Route>
-        <Route path='/examen10'>
-          <Examen10 />
-        </Route>
-      </div>
+      <Routes>
+        <Route exact path='/'element={ <Home />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/theorieexamens' element={ <Theorieexamens />} />
+        <Route path='/contact' element={<Contact />}/>
+          
+     
+        <Route path='/examen1' element={<Examen1 />} />
+        <Route path='/examen2' element={<Examen2 />} />
+        <Route path='/examen3' element={<Examen3 />} />
+        <Route path='/examen4' element={<Examen4 />} />
+        <Route path='/examen5' element={<Examen5 />} />
+        <Route path='/examen6' element={<Examen6 />} />
+        <Route path='/examen7' element={<Examen7 />} />
+        <Route path='/examen8' element={<Examen8 />} />
+        <Route path='/examen9' element={<Examen9 />} />
+        <Route path='/examen10' element={<Examen10 />} />
+      </Routes>
     </Router>
   )
 }
 
-export default App;
+export default App
